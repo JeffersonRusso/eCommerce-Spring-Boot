2f1af394ff27a3245c8500fe47096e517c9a7590
Jefferson Russo Silva <jefferson.russo@Outlook.com>
2021-05-22 15:48:48 -0300

    projeto base

---
+++ src/test/java/com/lojavirtual/logistica/LogisticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojavirtual.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
